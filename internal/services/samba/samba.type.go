e735b52ae57506f887b29881c947f33185f2b14e
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-12 21:53:16 +0700

    feat: add json tag in share type

--- internal/services/samba/samba.type.go
+++ internal/services/samba/samba.type.go
@@ -8,11 +8,11 @@ const (
 )
 
 type Share struct {
-	Path       string   `ini:"path"`
-	ReadOnly   Bool     `ini:"read only"`
-	Browsable  Bool     `ini:"browsable"`
-	GuestUsers []string `ini:"guest users"`
-	AdminUsers []string `ini:"admin users"`
+	Path       string   `ini:"path" json:"path"`
+	ReadOnly   Bool     `ini:"read only" json:"read_only"`
+	Browsable  Bool     `ini:"browsable" json:"browsable"`
+	GuestUsers []string `ini:"guest users" json:"guest_users"`
+	AdminUsers []string `ini:"admin users" json:"admin_users"`
 }
 
 type Shares map[string]Share
