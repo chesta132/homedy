a5f375d809635d43abf375fa4de620680870458d
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-13 10:16:41 +0700

    feat: add set permission and make dir with perm lib

--- internal/libs/iolib/io.lib.go
+++ internal/libs/iolib/io.lib.go
@@ -2,8 +2,13 @@ package iolib
 
 import (
 	"fmt"
+	"homedy/internal/libs/slicelib"
 	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func CopyFile(src, dst string) error {
@@ -33,5 +38,49 @@ func CopyFile(src, dst string) error {
 		return fmt.Errorf("failed to sync destination file: %w", err)
 	}
 
+	return nil
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func SetPermission(path string, permissions []int, recursive bool) error {
+	if len(permissions) != 3 {
+		return fmt.Errorf("%w: must be 3 items", ErrPermissionLength)
+	}
+	for permission := range permissions {
+		if permission > 7 || permission < 0 {
+			return fmt.Errorf("%w: %d", ErrPermissionNotKnown, permission)
+		}
+	}
+
+	if !Exists(path) {
+		return fmt.Errorf("%w: %s", os.ErrNotExist, path)
+	}
+
+	permissionSlcStr := slicelib.Map(permissions, func(i int, p int) string { return strconv.Itoa(p) })
+	permissionStr := strings.Join(append([]string{"0"}, permissionSlcStr...), "")
+	permissionInt, _ := strconv.Atoi(permissionStr)
+
+	if !recursive {
+		return os.Chmod(path, os.FileMode(permissionInt))
+	}
+
+	return filepath.WalkDir(path, func(p string, d fs.DirEntry, err error) error {
+		return os.Chmod(p, os.FileMode(permissionInt))
+	})
+}
+
+func MakeDirWithPerm(path string, permission []int) error {
+	err := os.MkdirAll(path, os.ModeDir)
+	if err != nil {
+		return fmt.Errorf("error while make dir all: %w", err)
+	}
+	err = SetPermission(path, permission, true)
+	if err != nil {
+		return fmt.Errorf("error while set permission: %w", err)
+	}
 	return nil
 }
