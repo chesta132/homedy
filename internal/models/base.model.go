40f8ace37fbb98f6f1e56a970a04060b3f9c1fd3
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-14 10:47:31 +0700

    feat: use gen_random_uuid instead of before create hook

--- internal/models/base.model.go
+++ internal/models/base.model.go
@@ -3,12 +3,11 @@ package models
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Base struct {
-	ID        string    `json:"id,omitempty" example:"479b5b5f-81b1-4669-91a5-b5bf69e597c6"`
+	ID        string    `json:"id" gorm:"default:gen_random_uuid()" example:"479b5b5f-81b1-4669-91a5-b5bf69e597c6"`
 	CreatedAt time.Time `json:"created_at,omitzero" gorm:"autoCreateTime;not null" example:"2006-01-02T15:04:05Z07:00"`
 	UpdatedAt time.Time `json:"updated_at,omitzero" gorm:"autoUpdateTime;not null" example:"2006-01-02T15:04:05Z07:00"`
 }
@@ -16,12 +15,4 @@ type Base struct {
 type BaseRecyclable struct {
 	Base
 	DeleteAt gorm.DeletedAt `json:"delete_at" gorm:"index" example:"2006-01-02T15:04:05Z07:00"`
-}
-
-func (b *Base) BeforeCreate(tx *gorm.DB) error {
-	if b.ID == "" {
-		b.ID = uuid.New().String()
-	}
-
-	return nil
 }
