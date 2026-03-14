4e5a8ea6a95f8bc5e9d94d2acb9f165b7259caf6
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-14 08:48:33 +0700

    fix: model regex doesn't compile

--- internal/libs/validatorlib/model.validator.go
+++ internal/libs/validatorlib/model.validator.go
@@ -6,8 +6,9 @@ import (
 )
 
 var (
-	usernameRegex = regexp.MustCompile(`^(?![0-9]+$)[a-zA-Z0-9_]{1,30}$|^([a-zA-Z0-9_](?:(?:[a-zA-Z0-9_]|(?:\.(?! \.))) {0,28}(?:[a-zA-Z0-9_]))?)$`)
-	passwordRegex = regexp.MustCompile(`^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$`)
+	usernameRegex = regexp.MustCompile(`^[a-zA-Z0-9_]([a-zA-Z0-9_.]{1,28}[a-zA-Z0-9_]|[a-zA-Z0-9_]?)$`)
+	passwordRegex = regexp.MustCompile(`^[A-Za-z\d@$!%*?&]{8,32}$`)
+	allDigits = regexp.MustCompile(`^[0-9]+$`)
 )
 
 func ValidateUsername(value any) bool {
@@ -16,7 +17,12 @@ func ValidateUsername(value any) bool {
 	if t.Kind() != reflect.String {
 		return false
 	}
-	return usernameRegex.MatchString(v.String())
+
+	vStr := v.String()
+	if !usernameRegex.MatchString(vStr) {
+		return false
+	}
+	return !allDigits.MatchString(vStr)
 }
 
 func ValidatePassword(value any) bool {
@@ -25,5 +31,23 @@ func ValidatePassword(value any) bool {
 	if t.Kind() != reflect.String {
 		return false
 	}
-	return passwordRegex.MatchString(v.String())
+
+	valStr := v.String()
+	if !passwordRegex.MatchString(valStr) {
+		return false
+	}
+
+	// go doesnt support this action
+	var hasLower, hasUpper, hasDigit bool
+	for _, c := range valStr {
+		switch {
+		case c >= 'a' && c <= 'z':
+			hasLower = true
+		case c >= 'A' && c <= 'Z':
+			hasUpper = true
+		case c >= '0' && c <= '9':
+			hasDigit = true
+		}
+	}
+	return hasLower && hasUpper && hasDigit
 }
