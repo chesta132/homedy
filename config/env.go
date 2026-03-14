1967710611137e694b5bc2e502917d170337876b
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-14 08:35:38 +0700

    fix: update reload env

--- config/env.go
+++ config/env.go
@@ -17,8 +17,8 @@ var (
 )
 
 func ReloadEnv() {
-	GO_ENV = os.Getenv("GO_ENV")
-	SERVER_PORT = os.Getenv("PORT")
+	GO_ENV = os.Getenv("HOMEDY_ENV")
+	SERVER_PORT = os.Getenv("HOMEDY_PORT")
 
 	DB_HOST = os.Getenv("HOMEDY_DB_PATH")
 	DB_PORT = os.Getenv("HOMEDY_DB_PORT")
@@ -26,6 +26,6 @@ func ReloadEnv() {
 	DB_PASS = os.Getenv("HOMEDY_DB_PASS")
 	DB_NAME = os.Getenv("HOMEDY_DB_NAME")
 
-	REFRESH_SECRET = os.Getenv("REFRESH_SECRET")
-	ACCESS_SECRET = os.Getenv("ACCESS_SECRET")
+	REFRESH_SECRET = os.Getenv("HOMEDY_REFRESH_SECRET")
+	ACCESS_SECRET = os.Getenv("HOMEDY_ACCESS_SECRET")
 }
