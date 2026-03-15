49c9680e4edc08f0c97ac96c97551f82e93d7482
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-15 14:25:11 +0700

    feat: add protect in route sign out

--- internal/routes/auth.route.go
+++ internal/routes/auth.route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"homedy/internal/handlers"
+	"homedy/internal/middlewares"
 	"homedy/internal/services"
 
 	"github.com/gin-gonic/gin"
@@ -10,8 +11,9 @@ import (
 func (rt *Router) RegisterAuth(group *gin.RouterGroup) {
 	authSvc := services.NewAuth(rt.repos.User(), rt.repos.Revoke())
 	h := handlers.NewAuth(authSvc)
+	amw := middlewares.NewAuth(rt.repos.Revoke())
 
 	group.POST("/signup", h.SignUp)
 	group.POST("/signin", h.SignIn)
-	group.POST("/signout", h.SignOut)
+	group.POST("/signout", amw.Protected(), h.SignOut)
 }
