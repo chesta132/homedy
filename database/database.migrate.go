06bb4a062a6ea5665e6ed62790bd39544010c88c
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-03-16 14:02:24 +0700

    feat: add chat models

--- database/database.migrate.go
+++ database/database.migrate.go
@@ -7,5 +7,8 @@ import (
 )
 
 func migrate(db *gorm.DB) error {
-	return db.AutoMigrate(&models.User{}, &models.Revoke{})
+	return db.AutoMigrate(
+		&models.User{}, &models.Revoke{},
+		&models.ChatRoom{}, &models.Message{}, &models.MessageRead{}, &models.ChatRoomMember{},
+	)
 }
