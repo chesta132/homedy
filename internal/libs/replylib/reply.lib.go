e914c003e30b50e31fa6cd38a866f60100796382
Chesta Ardiona <chestapanduganteng@gmail.com>
2026-04-03 20:10:40 +0700

    feat: add error not found in replylib.HandleError

--- internal/libs/replylib/reply.lib.go
+++ internal/libs/replylib/reply.lib.go
@@ -1,7 +1,10 @@
 package replylib
 
 import (
+	"errors"
+
 	"github.com/chesta132/goreply/reply"
+	"gorm.io/gorm"
 )
 
 func ErrorPayloadToErrorArg(err reply.ErrorPayload) (string, string, reply.ErrorOption, reply.ErrorOption) {
@@ -12,6 +15,10 @@ func HandleError(err error, rp *reply.Reply) {
 	if err, ok := err.(*reply.ErrorPayload); ok {
 		rp.Error(ErrorPayloadToErrorArg(*err)).FailJSON()
 		return
+	}
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		rp.Error(CodeNotFound, err.Error()).FailJSON()
+		return 
 	}
 	rp.Error(CodeServerError, err.Error()).FailJSON()
 }
